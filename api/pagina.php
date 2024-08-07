fc0ecf21025cefeae8b48df22b7d0c557c155db6
Bruna Tholl <bruna.tholl.ti@gmail.com>
2024-08-06 21:55:56 -0300

    Update pagina.php

--- api/pagina.php
+++ api/pagina.php
@@ -1,1 +1,15 @@
+<?php
 
+$sHtml = '<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>MInha pagina!</h1>
+</body>
+</html>';
+
+echo $sHtml;
